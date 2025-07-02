707cf830efa501e0cb96bbbb1ad904b7480ae165
Uijin <108450394+promisingJin@users.noreply.github.com>
2025-07-03 02:53:03 +0900

    update changes

---
+++ backend/apps/students/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Student(models.Model):
+    student_id = models.AutoField(primary_key=True)
+    class_id = models.IntegerField()
+    name = models.CharField(max_length=30)
+    birth_date = models.IntegerField()
+    gender = models.CharField(max_length=10, choices=(('male', 'male'), ('female', 'female')))
+
