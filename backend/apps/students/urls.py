4b6a6fc68fa7746462263903984dea43f48e0acd
maruiNia <maruinano@gmail.com>
2025-07-04 17:34:48 +0900

    modify urls.py add TokenObtainPairView & TokenRefreshView from rest_framework_simplejwt.view

--- backend/apps/students/urls.py
+++ backend/apps/students/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import StudentViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 
 router = DefaultRouter()
 router.register('students', StudentViewSet)
