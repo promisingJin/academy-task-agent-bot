dc635d1cf27f6f0d03a4eec41e29971a9c27c6de
maruiNia <maruinano@gmail.com>
2025-07-04 18:12:25 +0900

    modify models.py add class Teacher

--- backend/apps/students/models.py
+++ backend/apps/students/models.py
@@ -7,3 +7,10 @@ class Student(models.Model):
     birth_date = models.IntegerField()
     gender = models.CharField(max_length=10, choices=(('male', 'male'), ('female', 'female')))
 
+class Teacher(models.Model) :
+    teacher_id = models.IntegerField(primary_key=True)
+    age = models.IntegerField
+    name = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    sex = models.CharField(max_length=100)
+
